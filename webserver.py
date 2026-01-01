8eeff0e3c3a73d98bc9c72602ae838fa79be5704
Edward Zhang <ezhang2310@gmail.com>
2025-12-31 17:36:59 -0800

    Added use_reloader=false to webserver.py

--- webserver.py
+++ webserver.py
@@ -15,7 +15,7 @@ def health():
 def run():
     # Render requires using the PORT environment variable
     port = int(os.getenv('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, use_reloader=False)
 
 def keep_alive():
     t = Thread(target=run)
